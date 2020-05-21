5f1c31e7fdf1f093d4e193737ca548ed6fdaf595
Onuche Idoko <gee_gog@yahoo.com>
2020-05-21 21:54:29 +0300

    boc_linguistic review classification

---
+++ classification/boc_n_linguistic_model.py
@@ -0,0 +1,191 @@
+import ast
+import csv
+import time
+
+import numpy as np
+import pandas as pd
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import precision_recall_fscore_support
+from sklearn.model_selection import train_test_split
+
+
+class SentenceClassification:
+    def __init__(self):
+        df_dataset = pd.read_csv('../Review_Dataset/Gu_Dataset_features.csv', index_col=False)
+        self.review_dataset = pd.read_csv('../data/reviews.csv', index_col=False,
+                                          dtype={"class": "string"})
+        self.train_dataset, self.test_data = train_test_split(df_dataset, test_size=0.2)
+
+    def evaluate_model_on_test_set(self, ds_test, classify_model, vec, is_actual_test_review=False):
+
+        test_feature_dicts = []
+        true_labels = []
+
+        for index, row in ds_test.iterrows():
+            features = self.sentence_features(row)
+            test_feature_dicts.append(features)
+            true_labels.append(row['class'])
+
+        test_x = vec.transform(test_feature_dicts)
+
+        # print(true_labels)
+
+        # Predict the test data labels
+        pred_y = classify_model.predict(test_x)
+
+        if is_actual_test_review:
+            return pred_y
+        else:
+            _precision, _recall, f_score, support = precision_recall_fscore_support(true_labels, pred_y,
+                                                                                    labels=['E', 'P', 'R', 'B', 'N'])
+            return _precision, _recall, f_score
+
+    def model(self):
+
+        train_x, feature_transform, class_labels = self.generate_feature_matrix(self.train_dataset)
+
+        train_labels = np.array(class_labels)
+        lr_model = LogisticRegression(solver='lbfgs', multi_class='multinomial', C=.09)
+
+        lr_model.fit(train_x, train_labels)
+
+        return feature_transform, lr_model
+
+    def train_n_evaluate_on_test_data(self):
+
+        feature_transform, lr_model = self.model()
+        _precision, _recall, f_score = self.evaluate_model_on_test_set(self.test_data, lr_model, feature_transform)
+
+        return _precision, _recall, f_score
+
+    @staticmethod
+    def sentence_features(row):
+
+        sentence = ast.literal_eval((row['tagged_sent']))
+
+        # pos tag features
+        pos_tags_feature = '-'.join(tag for (word, tag) in sentence)
+
+        all_char_grams = ast.literal_eval(row['char_grams'])
+
+        features = {}
+
+        for char_gram in all_char_grams:
+            features['{}'.format(char_gram)] = True
+
+        # root word of a sentence
+
+        features['ROOT({})'.format(row['root'])] = True
+
+        features['{}'.format(pos_tags_feature)] = True
+
+        features['{}'.format(row['parse_tree'])] = True
+
+        features['{}'.format(row['dep_tree'])] = True
+
+        return features
+
+    def generate_feature_matrix(self, ds):
+        feature_dicts = []
+        class_labels = []
+
+        for index, row in ds.iterrows():
+            features = self.sentence_features(row)
+            feature_dicts.append(features)
+            class_labels.append(row['class'])
+
+        assert len(feature_dicts) == ds.shape[0]
+
+        vec = DictVectorizer()
+        feature_matrix = vec.fit_transform(feature_dicts)
+        return feature_matrix, vec, class_labels
+
+
+def evaluate():
+    _start_time = time.time()
+    sentence_classifier = SentenceClassification()
+    max_iteration = 10
+
+    average_precision_eval, average_recall_eval, average_fscore_eval = ([] for i in range(3))
+    average_precision_praise, average_recall_praise, average_fscore_praise = ([] for i in range(3))
+    average_precision_request, average_recall_request, average_fscore_request = ([] for i in range(3))
+    average_precision_bug, average_recall_bug, average_fscore_bug = ([] for i in range(3))
+    average_precision_others, average_recall_others, average_fscore_others = ([] for i in range(3))
+
+    with open('BoC_n_Linguistic_Model_Results.csv', 'w') as f:
+        writer = csv.writer(f, lineterminator='\n')
+        writer.writerow(['Iteration', 'P', 'R', 'F1', 'P', 'R', 'F1', 'P', 'R', 'F1', 'P', 'R', 'F1', 'P', 'R', 'F1'])
+
+        for i in range(0, max_iteration):
+            print('############Iteration # %d###############' % (i + 1))
+
+            # obj.SplitDataintoTrainTest()
+            precision, recall, fscore = sentence_classifier.train_n_evaluate_on_test_data()
+
+            average_precision_eval.append(precision[0])
+            average_recall_eval.append(recall[0])
+            average_fscore_eval.append(fscore[0])
+
+            average_precision_praise.append(precision[1])
+            average_recall_praise.append(recall[1])
+            average_fscore_praise.append(fscore[1])
+
+            average_precision_request.append(precision[2])
+            average_recall_request.append(recall[2])
+            average_fscore_request.append(fscore[2])
+
+            average_precision_bug.append(precision[3])
+            average_recall_bug.append(recall[3])
+            average_fscore_bug.append(fscore[3])
+
+            average_precision_others.append(precision[4])
+            average_recall_others.append(recall[4])
+            average_fscore_others.append(fscore[4])
+
+            lst = [i + 1, precision[0], recall[0], fscore[0], precision[1], recall[1], fscore[1]]
+            lst += [precision[2], recall[2], fscore[2], precision[3], recall[3], fscore[3]]
+            lst += [precision[4], recall[4], fscore[4]]
+
+            writer.writerow(lst)
+
+            print("Precision : %.3f, Recall : %.3f , Fscore : %.3f (feature evaluation)" % (
+                precision[0], recall[0], fscore[0]))
+            print("Precision : %.3f, Recall : %.3f , Fscore : %.3f (praise)" % (precision[1], recall[1], fscore[1]))
+            print("Precision : %.3f, Recall : %.3f , Fscore : %.3f (feature request)" % (
+                precision[2], recall[2], fscore[2]))
+            print("Precision : %.3f, Recall : %.3f , Fscore : %.3f (bug report)" % (precision[3], recall[3], fscore[3]))
+            print("Precision : %.3f, Recall : %.3f , Fscore : %.3f (others)" % (precision[4], recall[4], fscore[4]))
+
+            print('+++++++++++++++ITERATION FINISHED++++++++++++++++++++++++++++')
+
+        print("###########Average results for each class over 10 iterations####################")
+
+        print("Precision : %.3f, Recall : %.3f , Fscore : %.3f (feature evaluation)" % (
+            np.mean(average_precision_eval), np.mean(average_recall_eval), np.mean(average_fscore_eval)))
+        print("Precision : %.3f, Recall : %.3f , Fscore : %.3f (praise)" % (
+            np.mean(average_precision_praise), np.mean(average_recall_praise), np.mean(average_fscore_praise)))
+        print("Precision : %.3f, Recall : %.3f , Fscore : %.3f (feature request)" % (
+            np.mean(average_precision_request), np.mean(average_recall_request), np.mean(average_fscore_request)))
+        print("Precision : %.3f, Recall : %.3f , Fscore : %.3f (bug report)" % (
+            np.mean(average_precision_bug), np.mean(average_recall_bug), np.mean(average_fscore_bug)))
+        print("Precision : %.3f, Recall : %.3f , Fscore : %.3f (others)" % (
+            np.mean(average_precision_others), np.mean(average_recall_others), np.mean(average_fscore_others)))
+
+        print('The script finished in {0} seconds'.format(time.time() - _start_time))
+
+
+if __name__ == '__main__':
+    # evaluate()
+    start_time = time.time()
+    sentence_classifier = SentenceClassification()
+    feat_transform, model = sentence_classifier.model()
+    reviews = sentence_classifier.review_dataset
+    predictions = sentence_classifier.evaluate_model_on_test_set(reviews, model, feat_transform,
+                                                                 is_actual_test_review=True)
+
+    for index, pred in enumerate(predictions.tolist()):
+        reviews.at[index, 'class'] = pred
+
+    reviews.to_csv('../data/reviews_classified.csv', index=True)
+    print('done')
---
+++ classification/extract_boc_n_linguistic_features.py
@@ -0,0 +1,161 @@
+from os import listdir
+from os.path import isfile, join
+
+import nltk
+import pandas as pd
+import requests
+import spacy
+from nltk.tree import Tree
+
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+nlp = spacy.load('en_core_web_sm')
+url = 'http://localhost:9000/?'
+
+
+class ReviewFeatureExtractor:
+
+    def __init__(self):
+        self.df_dataset = pd.DataFrame(columns=('class', 'sentence'))
+
+    def extract_boc_n_linguistic_features(self):
+        dataset_path = "../Review_Dataset"
+
+        file_list = [f for f in listdir(dataset_path) if isfile(join(dataset_path, f))]
+
+        for file_path in file_list:
+            print(file_path)
+            file_full_path = dataset_path + "/" + file_path
+            df = pd.read_csv(file_full_path)
+            class_type = []
+            self.get_dataset(df=df, content_index=1)
+
+        self.df_dataset.to_csv('../Review_Dataset/Gu_Dataset_features.csv', index=True)
+
+    def extract_boc_n_linguistic_features_from_app_reviews(self):
+
+        file_full_path = "../data/org.catrobat.paintroid.csv"
+        df = pd.read_csv(file_full_path)
+
+        self.get_dataset(df=df, content_index=4)
+
+        self.df_dataset.to_csv('../data/reviews.csv', index=True)
+
+    @staticmethod
+    def clean_sentence(sent):
+        typos = [["im", "i m", "I m"], ['Ill'], ["cant", "cnt"], ["doesnt"], ["dont"], ["isnt"], ["didnt"], ["couldnt"],
+                 ["Cud"], ["wasnt"], ["wont"], ["wouldnt"], ["ive"], ["Ive"], ["theres"], ["awsome", "awsum", "awsm"],
+                 ["Its"], ["dis", "diz"], ["plzz", "Plz ", "plz", "pls ", "Pls", "Pls "], [" U ", " u "], ["ur"], ["b"],
+                 ["r"], ["nd ", "n", "&"], ["bt"], ["nt"], ["coz", "cuz"], ["jus", "jst"], ["luv", "Luv"], ["gud"],
+                 ["Gud"], ["wel"], ["gr8", "Gr8", "Grt", "grt"], ["Gr\\."], ["pics"], ["pic"], ["hav"], ["nic"],
+                 ["nyc ", "9ce"], ["Nic"], ["agn"], ["thx", "tks", "thanx"], ["Thx"], ["thkq"], ["Xcellent"], ["vry"],
+                 ["Vry"], ["fav", "favo"], ["soo", "sooo", "soooo"], ["ap"], ["b4"], ["ez"], ["w8"], ["msg"], ["alot"],
+                 ["lota"], ["kinda"], ["omg"], ["gota"]]
+        replacements = ["I'm", "i will", "can't", "doesn't", "don't", "isn't", "didn't", "couldn't", "Could", "wasn't",
+                        "won't", "wouldn't", "I have", "I have", "there's", "awesome", "It's", "this", "please", "you",
+                        "your", "be", "are", "and", "but", "not", "because", "just", "love", "good", "Good", "well",
+                        "great", "Great\\.", "pictures", "picture", "have", "nice", "nice", "Nice", "again", "thanks",
+                        "Thanks", "thank you", "excellent", "very", "Very", "favorite", "so", "app", "before", "easy",
+                        "wait", "message", "a lot", "lot of", "kind of", "oh, my god", "going to"]
+
+        sent_tokens = nltk.word_tokenize(sent)
+
+        new_sent = ""
+
+        for i, token in enumerate(sent_tokens):
+            found_type = False
+            for index, lst in enumerate(typos):
+                if token in lst:
+                    new_sent += replacements[index]
+                    if i < (len(sent_tokens)):
+                        new_sent += " "
+                        found_type = True
+                        break
+
+            if found_type == False:
+                new_sent += token
+                if i < (len(sent_tokens)):
+                    new_sent += " "
+
+        return (new_sent.strip())
+
+    def get_dataset(self, df, content_index):
+
+        for index, row in df.iterrows():
+            if isinstance(row[content_index], str):
+                clean_sent = self.clean_sentence(row[content_index])
+                clean_sent_words = nltk.word_tokenize(clean_sent)
+                sent_pos = nltk.pos_tag(clean_sent_words)
+
+                pos_tags_feature = '-'.join(tag for word, tag in sent_pos)
+
+                sent_str = ''.join([word for word, tag in sent_pos])
+                char_bi_gram = [sent_str[i:i + 2] for i in range(len(sent_str) - 2 + 1)]
+                char_tri_gram = [sent_str[i:i + 3] for i in range(len(sent_str) - 3 + 1)]
+                char_4_gram = [sent_str[i:i + 4] for i in range(len(sent_str) - 4 + 1)]
+
+                all_char_grams = char_bi_gram + char_tri_gram + char_4_gram
+
+                parsed_sent = nlp(clean_sent)
+
+                sent_root = None
+
+                for tok in parsed_sent:
+                    if tok.dep_ == "ROOT":
+                        sent_root = tok.orth_
+                        break
+
+                dep_tree_features = []
+
+                for token in parsed_sent:
+                    if token.dep_ == 'ROOT':
+                        dep_tree_features.append(str(token.dep_) + "-" + str(token.tag_))
+                        root_childs = [str(r.dep_) + "-" + str(r.tag_) for r in token.children]
+                        dep_tree_features.extend(root_childs)
+
+                output = requests.post(
+                    url + 'properties={"annotators": "parse", "outputFormat": "json","timeout": "500000"}',
+                    data={'data': clean_sent}).json()
+
+                result = output['sentences'][0]['parse']
+                parsetree = Tree.fromstring(result)
+                tree = parsetree[0]
+                parse_tree_feature = parsetree.label() + "-"
+                i = 1
+                for x in self.breadth_first(tree):
+                    if type(x) != str:
+                        if x.label() is not None:
+                            parse_tree_feature += x.label()
+                            i = i + 1
+                            if i > 5:
+                                break
+                            else:
+                                parse_tree_feature += "-"
+
+                data_row = {'sentence': clean_sent, 'tagged_sent': sent_pos, 'pos': pos_tags_feature,
+                            'char_grams': all_char_grams, 'root': sent_root,
+                            'dep_tree': '-'.join(dep_tree_features), 'parse_tree': parse_tree_feature,
+                            'class': row[0] if content_index == 1 else None}
+
+                self.df_dataset = self.df_dataset.append(data_row, ignore_index=True)
+
+    def breadth_first(self, tree, children=iter):
+        """Traverse the nodes of a tree in breadth-first order.
+        The first argument should be the tree root; children
+        should be a function taking as argument a tree node and
+        returning an iterator of the node's children.
+        """
+        yield tree
+        last = tree
+        for node in self.breadth_first(tree, children):
+            for child in children(node):
+                yield child
+                last = child
+            if last == node:
+                return
+
+
+if __name__ == '__main__':
+    obj = ReviewFeatureExtractor()
+    # obj.extract_boc_n_linguistic_features()
+    obj.extract_boc_n_linguistic_features_from_app_reviews()
